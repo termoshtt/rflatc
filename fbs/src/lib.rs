a6cbe03a506718f838f0ebbad8f363de3a952ac6
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-03-24 19:46:18 +0900

    Trial impl of get_data_table

--- fbs/src/lib.rs
+++ fbs/src/lib.rs
@@ -23,6 +23,16 @@ impl Buffer {
     }
 
     pub fn get_data_table(&self) -> &DataTable {
-        unimplemented!()
+        let p = self as *const _ as *const u8; // Drop size of file_identifier
+        let inc = self.root_table_offset / 8;
+        unsafe {
+            let table_ptr = p.offset(inc as isize);
+            let size = 0_usize;
+            // **CAUTION** Cast thin pointer to fat pointer
+            let fat_ptr =
+                std::mem::transmute::<(*const u8, usize), *const DataTable>((table_ptr, size));
+
+            &*fat_ptr
+        }
     }
 }

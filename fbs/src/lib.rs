d60ca3dc496ac208475e61e6fba6456e39e2ba2f
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-04-07 01:35:01 +0900

    Fix get_tables impl with comments

--- fbs/src/lib.rs
+++ fbs/src/lib.rs
@@ -84,46 +84,61 @@ impl Buffer {
         Ok(cstr.to_str()?)
     }
 
-    unsafe fn get_tables(&self) -> (&VTable, &Table) {
-        let ptr = &*self.raw as *const RawBuffer as *const u8;
-        let table_ptr = ptr.offset(self.raw.table_offset as isize);
-        // Read Table hader
-        let vtable_offset = {
-            let table_fat_ptr = mem::transmute::<(*const u8, usize), *const Table>((
-                table_ptr, 0, /* DUMMY value */
+    pub fn get_tables(&self) -> (&VTable, &Table) {
+        unsafe {
+            // CAUTION: almost every lines are unsafe :<
+
+            let ptr = &*self.raw as *const RawBuffer as *const u8;
+            let table_ptr = ptr.offset(self.raw.table_offset as isize);
+            // Read vtable offset from Table hader
+            let vtable_offset = {
+                // FIXME Revise to portable way
+                // Thin ptr to fat ptr transmute is not assured
+                let table_fat_ptr = mem::transmute::<(*const u8, usize), *const Table>((
+                    table_ptr,
+                    0, /* DUMMY size (since the length of trailing length of `data` is unknown here) */
+                ));
+                (*table_fat_ptr).vtable_offset
+            };
+
+            let vtable_ptr = (table_ptr as *const u8).offset(-vtable_offset as isize);
+
+            // Read vtable header
+            let (vtable_length, table_length) = {
+                // FIXME Revise to portable way
+                // Thin ptr to fat ptr transmute is not assured
+                let vtable_fat_ptr = mem::transmute::<(*const u8, usize), *const VTable>((
+                    vtable_ptr, 0, /* DUMMY size */
+                ));
+                (
+                    (*vtable_fat_ptr).vtable_length,
+                    (*vtable_fat_ptr).table_length,
+                )
+            };
+
+            // TODO Validate the table/vtable length input if these go in the buffer memory
+
+            let vtable: &VTable = &*mem::transmute::<(*const u8, usize), *const VTable>((
+                vtable_ptr,
+                (vtable_length / 2 - 2) as usize, // vtable_length is a length as a &[u8]
+                                                  // - divide by 2 to match &[u16]
+                                                  // - sub 2 for vtable_length(u16) and table_length(u16)
             ));
-            (*table_fat_ptr).vtable_offset
-        } as isize;
 
-        let vtable_ptr = (table_ptr as *const u8).offset(vtable_offset);
+            let table: &Table = &*mem::transmute::<(*const u8, usize), *const Table>((
+                table_ptr,
+                (table_length - 4) as usize,
+            ));
 
-        // Read vtable header
-        let (vtable_length, table_length) = {
-            let vtable_fat_ptr =
-                mem::transmute::<(*const u8, usize), *const VTable>((vtable_ptr, 0));
-            (
-                (*vtable_fat_ptr).vtable_length,
-                (*vtable_fat_ptr).table_length,
-            )
-        };
-
-        let vtable: &VTable = &*mem::transmute::<(*const u8, usize), *const VTable>((
-            vtable_ptr,
-            (vtable_length - 4) as usize,
-        ));
-
-        let table: &Table = &*mem::transmute::<(*const u8, usize), *const Table>((
-            table_ptr,
-            (table_length - 4) as usize,
-        ));
-
-        (vtable, table)
+            (vtable, table)
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::{fs, io::Read};
 
     const TEST_HEADER: &'static [u8] = &[
         0x00, 0x01, 0x00, 0x00, 'N' as u8, 'O' as u8, 'O' as u8, 'B' as u8, '\0' as u8,
@@ -139,5 +154,18 @@ mod tests {
     fn test_identifier() {
         let buf = Buffer::copy_from_slice(&TEST_HEADER);
         assert_eq!(buf.file_identifier().unwrap(), "NOOB");
+    }
+
+    #[test]
+    fn read_example_buffer() {
+        let mut f = fs::File::open("example.bin").unwrap();
+        let mut buf = Vec::new();
+        f.read_to_end(&mut buf).unwrap();
+        let fb = Buffer::copy_from_slice(&buf);
+
+        let (vtable, table) = unsafe { fb.get_tables() };
+        println!("vtable = {:?}", vtable);
+        println!("table  = {:?}", table);
+        panic!("!!")
     }
 }
